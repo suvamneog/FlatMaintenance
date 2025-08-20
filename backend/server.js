fd8dca4d57442572fb851ba31aac7f12190f45e1
suvamneog <neogsuvam@gmail.com>
2025-08-20 15:49:55 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,11 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  credentials: true,
+}));
+
 app.use(express.json());
 
 // MongoDB connection
